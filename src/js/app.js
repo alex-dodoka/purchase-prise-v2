c9b2895bc74970ac176520c5eb35d278ca89429f
alex-dodoka <neksus.92@mail.ru>
2017-07-02 15:53:25 +0300

    Working getPercentages function.

--- src/js/app.js
+++ src/js/app.js
@@ -5,26 +5,26 @@ function getRes() {
         a = parseFloat($('#a').val()),
         b = parseFloat($('#b').val()),
         x = parseFloat($('#x').val()) / 100,
-        y = parseFloat($('#y').val()) / 100;
+        y = +$('#y').val() / 100;
+
 
     if (a > b) {
         alert(`А must be less than  B`);
 
     }
-    if (n > a) {
-        getPercentages(n, x);
-    } else if (n > b) {
+    if (n > b) {
         getPercentages(n, y);
+    } else if (n > a) {
+        getPercentages(n, x);
     } else {
-        printRes(n);
+        printRes(n)
     }
-
 
 }
 
 
 function getPercentages(n, discount) {
-    return printRes(n - (discount * n));
+    return printRes(n - n * discount);
 }
 
 function printRes(num) {
